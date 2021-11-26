482fcf1c7be7f5b3bf9efc9d273b699b6fe37c9e
dalessi <danialessi93@hotmail.com>
2021-11-26 21:58:58 +1100

    Hotfixes and debug

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,7 @@
 let players = ["Player 1", "Player 2"];
 let playerTurn = players[Math.floor(Math.random() * 2)];
 let systemMessage = document.getElementsByClassName("systemMessage")[0];
-systemMessage.textContent = playerTurn + " Starts!";
+systemMessage.textContent = playerTurn + ", \r\n Starts!";
 
 // Event listener when we click an element within the parent element #gameGrid.
 const tileChoice = document.querySelector("#gameGrid");
@@ -23,12 +23,12 @@ function playerSymbol(event){
     if (playerTurn == players[0]){
         event.target.textContent = "X";
         event.target.style.color = "rgba(255, 93, 93, 1)";
-        systemMessage.textContent = players[1] + ", Turn!";
+        systemMessage.textContent = players[1] + ", \r\n Turn!";
         return playerTurn = players[1];
     } else if (playerTurn == players[1]){
         event.target.textContent = "O";
         event.target.style.color = "rgba(92, 189, 255, 1)"
-        systemMessage.textContent = players[0] + ", Turn!";
+        systemMessage.textContent = players[0] + ", \r\n Turn!";
         return playerTurn = players[0];
     }
 };
@@ -56,7 +56,7 @@ function checkWinner(){
         ||
         selectID[3].textContent === "X" && selectID[4].textContent === "X" && selectID[5].textContent === "X"
         ){
-            systemMessage.textContent = players[0] + " Wins!";
+            systemMessage.textContent = players[0] + ", \r\n Wins!";
             // Update ScoreBoard content
             document.getElementsByClassName("scoreBoard")[0].textContent = parseInt(document.getElementsByClassName("scoreBoard")[0].textContent) + 1;
             //This will stop the event listener
@@ -75,14 +75,14 @@ function checkWinner(){
         ||
         selectID[3].textContent === "O" && selectID[4].textContent === "O" && selectID[5].textContent === "O"
         ){
-            systemMessage.textContent = players[1] + " Wins!";
+            systemMessage.textContent = players[1] + ", \r\n Wins!";
             // Update ScoreBoard content
             document.getElementsByClassName("scoreBoard")[1].textContent = parseInt(document.getElementsByClassName("scoreBoard")[1].textContent) + 1;
             //This will stop the event listener
             tileChoice.removeEventListener("click", playerTileChoice);
             
         } else if (selectID.length === selectedTiles.length){
-            systemMessage.textContent = "Is a Draw!";
+            systemMessage.textContent = "Is a\r\n Draw!";
 
     };
 };
@@ -93,7 +93,7 @@ function restartGame(){
         gridChoice[i].textContent = "";
     }
     tileChoice.addEventListener("click", playerTileChoice);
-    systemMessage.textContent = playerTurn + " Starts!";
+    systemMessage.textContent = playerTurn + ", \r\n Starts!";
 };
 
 function displayNameWindow(){
@@ -107,23 +107,37 @@ function hideNameWindow(){
 function changeName(){
     //If input is not empty, change name to input value
     if (document.getElementsByTagName("input")[0].value != ""){
+        if (document.getElementsByTagName("input")[0].value === document.getElementsByTagName("input")[1].value || document.getElementsByTagName("input")[0].value === document.getElementsByClassName("player")[1].textContent){
+            document.getElementsByTagName("h4")[0].textContent = "Name taken, try something else";
+            document.getElementsByTagName("h4")[0].style.color = "red";
+            return;
+        } else {
         players[0] = document.getElementsByTagName("input")[0].value;
         document.getElementsByClassName("player")[0].textContent = players[0];
         document.getElementsByTagName("input")[0].value = "";
     }
+    }
     if (document.getElementsByTagName("input")[1].value != ""){
+        if (document.getElementsByTagName("input")[1].value === document.getElementsByTagName("input")[0].value || document.getElementsByTagName("input")[1].value === document.getElementsByClassName("player")[0].textContent){
+            document.getElementsByTagName("h4")[0].textContent = "Name taken, try something else";
+            document.getElementsByTagName("h4")[0].style.color = "red";
+            return;
+        } else {
         players[1] = document.getElementsByTagName("input")[1].value;
         document.getElementsByClassName("player")[1].textContent = players[1];
         document.getElementsByTagName("input")[1].value = "";
+        }
     }
+    document.getElementsByTagName("h4")[0].textContent = "Please choose your name";
+    document.getElementsByTagName("h4")[0].style.color = "black";
     // Close changeName UI
     hideNameWindow();
     //Change name mid-game without jumping/missing turns.
     if (playerTurn === "Player 1"){
-        systemMessage.textContent = players[0] + ", Turn!";
+        systemMessage.textContent = players[0] + ", \r\n is Next!";
         return playerTurn = players[0];
     } else if (playerTurn === "Player 2"){
-        systemMessage.textContent = players[1] + ", Turn!";
+        systemMessage.textContent = players[1] + ", \r\n is Next!";
         return playerTurn = players[1]
     }
 };
